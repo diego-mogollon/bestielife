796f258d883c7618c7c0d911ee7588cdc86cfbe3
diego mogollon <diego.mogollon.solano@gmail.com>
2021-02-25 21:41:25 +1100

    created add new event and added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
-
+import { initFlatpickr } from "../plugins/flatpickr";
+document.addEventListener("turbolinks:load", () => {
+initFlatpickr();
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
