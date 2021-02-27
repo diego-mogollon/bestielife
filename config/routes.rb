770e08c014d6b637c210152b9c1fbbacc4bc6e75
Aparna Achuthan <appu4ever@gmail.com>
2021-02-28 10:02:42 +1100

    CRUD for calendar and header view

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'photos/show'
   devise_for :users, controllers: { registrations: "registrations" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   authenticated :user do
     root to: 'pages#dashboard', as: :authenticated_root
   end
   root to: 'pages#home'
+
   resources :pets, only: [:new, :create] do 
     member do
       post :add_picture
       delete :remove_picture
     end
   end
+
   #FRIENDS ROUTES
   post '/pets/:id', to: 'friends#search_friend', as: 'search_friend'
   # post '/pets/:id', to: 'friends#reject_friend', as: 'reject_friend'
@@ -21,17 +22,23 @@ Rails.application.routes.draw do
   get "/friends/search" => "friends/search"
   post "/friends/search" => "friends/search"
   resources :friends, only: [:new, :index, :create]
+
   #CALENDAR ROUTES
-  get 'calendar', to: 'pages#calendar'
   resources :events
+
+  #EXPENSES ROUTES
   resources :expenses do  
    resources :expense_types
   end
+
   resources :budgets do  
     resources :expense_types
-   end
+  end
+
+  # EXPLORE ROUTES
   resources :places, only: [:index, :show] do
     resources :favourite_places, except: [:edit, :update]
   end
+
   resources :photos, only: [:show] 
 end
