23b1f1400c6e25d3fe8f8020178c741ce1f6a165
Aparna Achuthan <appu4ever@gmail.com>
2021-02-28 12:36:46 +1100

    Basic styling for calendar

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Bestielife
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.beginning_of_week = :sunday
   end
 end
