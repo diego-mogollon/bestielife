e8f7a9adf78444ad18f8c857f5216a80e1e443e2
diego mogollon <diego.mogollon.solano@gmail.com>
2021-03-02 18:19:34 +1100

    committing before pg uninstall

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,17 +2,17 @@ import consumer from "./consumer"
 
 // consumer.subscriptions.create("ChatroomChannel", {
 //   connected() {
-//     // Called when the subscription is ready for use on the server
-//   },
+    // Called when the subscription is ready for use on the server
+  },
 
-//   disconnected() {
-//     // Called when the subscription has been terminated by the server
-//   },
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
 
-//   received(data) {
-//     // Called when there's incoming data on the websocket for this channel
-//   }
-// });
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
 
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
