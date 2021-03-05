f51549232f954d5416e8b4b3d808a52914451b96
diego mogollon <diego.mogollon.solano@gmail.com>
2021-03-05 21:11:26 +1100

    completed chatroom routes for all chatrooms within Index

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,8 @@
 class ChatroomsController < ApplicationController
+    def index
+        @chatrooms = Chatroom.all
+        # @chatroom = Chatroom.find(params[:id])
+    end
     def show
         @chatroom = Chatroom.find(params[:id])
         @message = Message.new
