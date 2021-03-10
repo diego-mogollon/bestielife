017a4153b4faa74cf16a54c28466ec4903a2c6d0
diego mogollon <diego.mogollon.solano@gmail.com>
2021-03-11 00:10:03 +1100

    added js function for photo upload - pending fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,26 +31,15 @@ window.dispatchMapsEvent = function (...args) {
   //   window.dispatchEvent(event);
 };
 
-const addPhoto = () => {
-  // we select the photo input
-  const input = document.getElementById('add-photo');
-  if (input) {
-    // we add a listener to know when a new picture is uploaded
-    input.addEventListener('change', () => {
-      // we call the displayPreview function (who retrieve the image url and display it)
-      displayPreview(input);
-    })
-  }
-}
+const add = document.querySelector('#submit-photo');
+const submit = document.querySelector('#submit-photo');
 
-const displayPreview = (input) => {
-  if (input.files && input.files[0]) {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      document.getElementById('submit-photo').src = event.currentTarget.result;
-    }
-    reader.readAsDataURL(input.files[0])
-    document.getElementById('submit-photo').classList.remove('hidden');
+const addPhoto = () => {
+  if (add) {
+    add.addEventListener('change', () => {
+      submit.style.display = "";
+      ;
+    })
   }
 }
 export { addPhoto };
