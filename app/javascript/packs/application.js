eeb54d6ebf4d30baffb3cf0a728bac5c9614ff42
diego mogollon <diego.mogollon.solano@gmail.com>
2021-03-10 23:45:04 +1100

    working on js solution for image upload and hiding submit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,3 +30,27 @@ window.dispatchMapsEvent = function (...args) {
   //   event.args = args;
   //   window.dispatchEvent(event);
 };
+
+const addPhoto = () => {
+  // we select the photo input
+  const input = document.getElementById('add-photo');
+  if (input) {
+    // we add a listener to know when a new picture is uploaded
+    input.addEventListener('change', () => {
+      // we call the displayPreview function (who retrieve the image url and display it)
+      displayPreview(input);
+    })
+  }
+}
+
+const displayPreview = (input) => {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      document.getElementById('submit-photo').src = event.currentTarget.result;
+    }
+    reader.readAsDataURL(input.files[0])
+    document.getElementById('submit-photo').classList.remove('hidden');
+  }
+}
+export { addPhoto };
